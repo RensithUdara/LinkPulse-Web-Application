eb4184a9e406b11b52e28e0aa7176fb0c88ba387
RensithUdara <rensithudaragonalagoda@gmail.com>
2026-08-27 23:10:48 +0530

    fix: update BeforeCreate method signature to use *gorm.DB

--- internal/model/user.go
+++ internal/model/user.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type User struct {
@@ -14,7 +15,7 @@ type User struct {
 	UpdatedAt    time.Time `json:"updated_at"`
 }
 
-func (u *User) BeforeCreate(_ interface{}) error {
+func (u *User) BeforeCreate(_ *gorm.DB) error {
 	if u.ID == uuid.Nil {
 		u.ID = uuid.New()
 	}
